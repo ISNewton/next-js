5bb88e98cdd70e4bfdd1d095aeee0129676c8cf1
ISNewton <ashrfalhag.11@gmail.com>
2023-04-07 13:50:14 +0200

    fix app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import Nav from "../components/layout/nav"
+import { SessionProvider } from "next-auth/react"
 
 const MyApp = ({ Component, pageProps }) => {
 
     return (
+        <SessionProvider session={pageProps.session} >
+
         <Nav>
             <Component {...pageProps} />
         </Nav>
+        </SessionProvider>
+
     )
 }
 
